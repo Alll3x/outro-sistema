6442e03410cfd0c2d4a0a272455585a3083bec41
All3x <acprelle@gmail.com>
2023-01-24 10:32:30 -0300

    added and fixed vehicle registration system

--- app.js
+++ app.js
@@ -61,33 +61,55 @@
 
   // PAGINA CADASTRAR VEICULO NO USUARIO
   app.get('/cadastrarVeiculo/:id', async(req, res) =>{
-    res.render('cadastroVeiculos')
+    const result = await db.query(myQuery.SELECT_USERDATA_ADDRESS_ID(req.params.id))
+    const vehicle = await db.query(myQuery.SELECT_VEHICLES_BY_USERID(req.params.id))
+    res.render('cadastroVeiculos', { dados:result[0], vehicles: vehicle[0] })
 
   })
 
-  //CADASTRAR EM BANCO
-  app.post('/cadastrar', async(req, res) =>{
-      const newAddress = await Address.create({
-        cep: req.body.cep,
-        rua: req.body.rua,
-        numero: req.body.numero,
-        bairro: req.body.bairro,
-        cidade: req.body.cidade,
-        uf: req.body.uf
-      })
-      await User.create({
-        nome: req.body.nome,
-        telefone: req.body.telefone,
-        idEndereco: newAddress.id
+//CADASTRAR EM BANCO
+  //USUARIO
+    app.post('/cadastrar', async(req, res) =>{
+        const newAddress = await Address.create({
+          cep: req.body.cep,
+          rua: req.body.rua,
+          numero: req.body.numero,
+          bairro: req.body.bairro,
+          cidade: req.body.cidade,
+          uf: req.body.uf
+        })
+        await User.create({
+          nome: req.body.nome,
+          telefone: req.body.telefone,
+          idEndereco: newAddress.id
+        }).then(()=>{
+            return res.status(200).redirect('/usuariosCadastrados')
+        }).catch((err)=>{
+          return res.status(400).json({
+            erro: true,
+            mensagem: "Usuário não cadastrado " + err
+          })
+        })
+    })
+  
+  //VEICULO
+    app.post('/cadastrarVeiculos/:idUser', async(req, res) =>{
+      await Vehicle.create({
+        marca: req.body.marca,
+        modelo: req.body.modelo,
+        ano: req.body.ano,
+        placa: req.body.placa,
+        anotacoes: req.body.anotacoes,
+        idUsuario: req.params.idUser
       }).then(()=>{
-          return res.status(200).redirect('/usuariosCadastrados')
-      }).catch((err)=>{
+        return res.status(200).redirect(`/cadum/${req.params.idUser}`)
+      }).catch((err)=> {
         return res.status(400).json({
-          erro: true,
-          mensagem: "Usuário não cadastrado " + err
+        erro: true,
+        mensagem: "Veiculo não cadastrado " + err
         })
       })
-  })
+    })
 
   //deletar cadastro
   app.get('/deletar/:id', async(req, res)=>{
--- sql/userQueries.js
+++ sql/userQueries.js
@@ -1,4 +1,4 @@
-const SELECT_USERDATA = () => {
+  const SELECT_USERDATA = () => {
   return(
     `SELECT
       u.id AS IdUsuario,
@@ -8,7 +8,7 @@ const SELECT_USERDATA = () => {
       ofmeclara.users u`
   )};
 
-const SELECT_USERDATA_ADDRESS_ID = (id) => {
+  const SELECT_USERDATA_ADDRESS_ID = (id) => {
   return(
     `SELECT
       u.id AS IdUsuario,
@@ -46,7 +46,22 @@ const SELECT_USERDATA_ADDRESS_ID = (id) => {
         ofmeclara.users u
       JOIN 
         ofmeclara.addresses a ON u.idEndereco = a.id`
-    )};
+  )};
+
+  const SELECT_VEHICLES_BY_USERID = (id) =>{
+    return(
+      `SELECT 
+      v.id as idVeiculo,
+      v.marca, 
+      v.modelo,
+      v.ano,
+      v.placa,
+      v.anotacoes,
+      v.idUsuario
+      FROM ofmeclara.vehicles v
+      WHERE v.idUsuario = ${id}`
+    )
+  }
 
 module.exports = {
   //Retorna apenas nome e telefone de todos os usuários
@@ -55,4 +70,6 @@ module.exports = {
   SELECT_USERDATA_ADDRESS_ID,
   //Retorna todos os dados de todos os usuários
   SELECT_USERDATA_ADDRESS,
+  //Retorna todos os veículos cadastrados com base no id do usuario
+  SELECT_VEHICLES_BY_USERID,
 }
