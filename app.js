1468b0b9364df4d0fca501f9e7f04e75ac9f330d
All3x <acprelle@gmail.com>
2023-03-08 16:25:05 -0300

    'SomeCorrections'

--- app.js
+++ app.js
@@ -227,7 +227,7 @@
     //CONCLUIR TICKET E COLOCAR GARANTIA
        app.post('/concluir/:idTicket', async(req,res)=>{
         await db.query(ticketQuery.UPDATE_TICKET_WARRANTY_STATUS_BY_IDTICKET(req.params.idTicket, req.body.garantia))
-         res.redirect(`/pdf/${req.params.idTicket}`)
+        res.redirect(`/pdf/${req.params.idTicket}`)
       })
 
   //DELETE
